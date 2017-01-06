e1492f706e4f3fceafff5e093933de1f1f8b9fcc
Mark Goho <markgoho@gmail.com>
2017-01-06 14:00:09 -0500

    Added shopping list service

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'rb-shopping-list-add',
@@ -9,6 +10,7 @@ export class ShoppingListAddComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
