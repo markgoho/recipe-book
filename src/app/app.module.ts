259549a4d9eb5dcaf126c5faf5f4dbfadf9b10d6
Mark Goho <markgoho@gmail.com>
2017-01-12 09:05:24 -0500

    Core module for exports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { routing } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -6,24 +5,23 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { DropdownDirective } from './dropdown.directive';
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { HomeComponent } from './home.component';
+import { CoreModule } from './core.module';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    DropdownDirective,
-    HomeComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     routing,
-    ShoppingListModule
+    ShoppingListModule,
+    CoreModule
   ],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
